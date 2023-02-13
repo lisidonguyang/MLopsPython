af87174dba1b35799d66367786f7ff63f87ef254
Justin HANSZ-MERCIER <justin@MacBook-Air-de-Justin.local>
2023-02-13 15:46:15 +0100

    test  commit

--- demo_flake8.py
+++ demo_flake8.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+from pathlib import Pathkdldf
 print("hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world")
 package_dir = Path(__file__).parent.absolute()
 print(package_dir)
