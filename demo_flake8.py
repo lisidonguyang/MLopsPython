f8db195938a92f13a2f5c63d1d4968f2998b0bb6
lisidonguyang <lisidonguyang@github.com>
2023-02-17 21:32:30 +0100

    Initial commit

--- demo_flake8.py
+++ demo_flake8.py
@@ -1,4 +1,7 @@
 from pathlib import Path
-print("hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world")
+
+print(
+    "hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world"
+)
 package_dir = Path(__file__).parent.absolute()
 print(package_dir)
